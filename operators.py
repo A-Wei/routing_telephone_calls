2c394891c5bdcef0de4e8ea9879fdc10656c97fb
Wei Wei <wei@hem.com>
2019-08-23 19:35:43 +0200

    Update functions
    - Change the operator's data into list of tuple
    - Replace find_prefix method with .startwith() python build in method.
    - Rename and rewrite method `best_price` to `best_operator_and_price`
    - Takes inpurt from command line or mannul input.

--- operators.py
+++ operators.py
@@ -1,21 +1,8 @@
-Operator_A = {
-    '1': 0.9,
-    '268': 5.1,
-    '46': 0.17,
-    '4620': 0.0,
-    '468': 0.15,
-    '4631': 0.15,
-    '4673': 0.9,
-    '46732': 1.1,
-}
+import argparse
 
-Operator_B = {
-    '1': 0.92,
-    '44': 0.5,
-    '46': 0.2,
-    '467': 1.0,
-    '48': 1.2,
-}
+Operator_A = [(1, 0.9), (268, 5.1), (46, 0.17), (4620, 0.0), (468, 0.15), (4631, 0.15), (4673, 0.9), (46732, 1.1)]
+Operator_B = [(1, 0.92), (44, 0.5), (46, 0.2), (467, 1.0), (48, 1.2)]
+
 ops = {
     'Operator_A': Operator_A,
     'Operator_B': Operator_B,
@@ -24,86 +11,44 @@ ops = {
 
 class Operators():
     """Handls operator to find the best match"""
-    def __init__(self):
-        pass
+    def __init__(self, number):
+        self.number = number
 
-    def find_prefix(self, phone_number, prefixs):
-        """Find the longest match between prefixs and phone number"""
-        # Get the total number of prefixs
-        n = len(prefixs)
-
-        # For each prefix in the list
-        for i in range(0,n-1):
-            #Get the actual prifix
-            prefix = prefixs[i]
-
-            # Calcualte the prefix length
-            l = len(prefix)
-
-            # The the same length as prefix from phone number
-            short_phone_number = phone_number[0:l]
-
-            # Compare prefix and short phone number
-            if prefix == short_phone_number:
-                return prefix
-            else:
-                pass
-
-        return None
-
-
-    def best_price(self, phone_number):
-        """Find the best price to call the given phone number"""
-        operator = ''
+    def best_operator_and_price(self):
         best_price = None
+        best_operator = None
 
-        # Get all operators in a list
-        for name, dict_ in ops.items():
-            # Convert dict keys to a list
-            prefixs = list(dict_.keys())
+        for op_name, op_value in ops.items():
+            op_value.sort(reverse=True)
+            for i in op_value:
+                if str(self.number).startswith(str(i[0])):
+                    if best_price is None or best_price > i[1]:
+                        best_price = i[1]
+                        best_operator = op_name
+                    break
 
-            # Sort the list of keys by length in reversed order, prefixs saved as string
-            prefixs.sort(key=len, reverse=True)
-
-            # Find the best prifix of phone number by the given operator
-            prefix = self.find_prefix(phone_number, prefixs)
-
-            # Try to find the price of the prefix return, set price to None if can't find the prefix
-            try:
-                price = dict_[prefix]
-            except KeyError:
-                price = None
-
-            # Evaluate and compare the price with the best_price
-            if best_price is None:
-                best_price = price
-                operator = name
-            elif best_price > price:
-                best_price = price
-                operator = name
-            else:
-                pass
-
-        return operator, best_price
-
+        return best_operator, best_price
 
 def main():
-    # Save the given phone number into a variable
-    phone_number = input("Phone Number: ")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", help="Number to call!", type=int)
+    args = parser.parse_args()
+
+    if args.n is None:
+        phone_number = input("Phone Number: ")
+    else:
+        phone_number = str(args.n)
 
     # Validate the given phone number, if too short, print a message
     if len(phone_number) < 8:
         print("Given phone number is too short, minimum 8 digits")
+        raise Exception("Phone number too short!")
 
     # Save the returned operator and best_price
-    operator, best_price = Operators().best_price(phone_number)
-
-    # Validate the operator
+    operator, best_price = Operators(phone_number).best_operator_and_price()
     if operator is None:
-        # If none of the operator can handle the phone, return a message.
-        print(f"Can't find an operator to call this number: {phone_number}")
+        print("No operator found")
     else:
-        # Return the operator with best price find
         print(f"{operator} has the best price ${best_price}/min for number {phone_number}")
 
 
